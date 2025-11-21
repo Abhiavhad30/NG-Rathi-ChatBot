4f5a59aaa45046573f75ae44567203ee11470a9b
Abhiavhad30 <abhijitavhad727@gmail.com>
2025-11-21 17:45:00 +0530

    change in  proxy.py file

--- proxy.py
+++ proxy.py
@@ -9,8 +9,11 @@ def health():
 
 @app.route("/webhooks/rest/webhook", methods=["POST"])
 def forward():
-    response = requests.post("http://localhost:10000/webhooks/rest/webhook", json=request.get_json())
-    return jsonify(response.json())
+    try:
+        response = requests.post("http://localhost:10000/webhooks/rest/webhook", json=request.get_json())
+        return jsonify(response.json())
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(10000))
+    app.run(host="0.0.0.0", port=5000)
